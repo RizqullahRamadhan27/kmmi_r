6c792e7cdba12df48bd99d2634af52ab5247526a
Rizqullah Ramadhan <111201912378@mhs.dinus.ac.id>
2021-08-09 16:32:48 +0700

    Add List

--- test2.r
+++ test2.r
@@ -1,14 +1,17 @@
 teks1 = "Rizqullah Ramadhan M"
-teks2 = "A11.2019.12378"
+teks2 = "Selamat datang di dunia R!!"
+teks3 = "Anda akan terkesima dengan kehebatannyaa!"
 
 teks1
 teks2
+teks3
+
 
 pick = list("Sate","Soto","Nasi Goreng")
 pick
 
 q = 5
-w = 3
+w = 1
 
 z = q + w
 z
